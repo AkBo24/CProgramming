82f233092577c95990827ead633733c9b2433179
AkBo24 <akshaybodla21@gmail.com>
2020-06-01 11:52:42 -0700

    Count str len w/ pointers

--- CProgramming/src/Pointers/ArithmeticPointers.c
+++ CProgramming/src/Pointers/ArithmeticPointers.c
@@ -8,7 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-arithmeticPointers() {
+int strLen(char * pStr);
+
+void arithmeticPointers() {
 
 	puts("Since pointers point to a memory address we can inc or dec as such:");
 	puts("ptr++ or ptr--");
@@ -22,7 +24,23 @@ arithmeticPointers() {
 		sum += *pSum;
 	}
 
-	printf("\nSum of array: %d", sum);
+	printf("\nSum of array: %d\n", sum);
 	puts("Refer to ArithmeticPointers.c for the code");
 
+	char str[] = "12345abcd";
+
+	int sLen = strLen(&str);
+	printf("\nCalculate the length of a string: \"12345abcd\" => %d", sLen);
+}
+
+int strLen(char * pStr) {
+	int len = 0;
+
+	while(*pStr) { //will terminate when pStr points to 0 (ascii null terminator)
+		len++;
+//		printf("%c\n", *pStr);
+		pStr++;
+	}
+//	puts(len);
+	return len;
 }
